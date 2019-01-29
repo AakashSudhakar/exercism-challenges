584cdc0dbb8359d3990fc43b380128711a3c7a83
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-29 14:46:41 -0800

    Committing miscellaneous changes
    Commented code for clarity

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -6,6 +6,10 @@ import "errors"
 func Distance(a, b string) (int, error) {
 	var HammingError error
 	var HammingDistance int
+
+	// Checks if there is length mismatch across genomes
+	// If length matches, checks for letter mismatches
+	// If letter mismatch, increment Hamming distance
 	if len(a) != len(b) {
 		HammingError = errors.New("Length mismatch")
 	} else {
