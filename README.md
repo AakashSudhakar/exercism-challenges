e317fd67ba7a7f32587246a448b02a91423e76b3
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-02-17 00:58:57 -0800

    Passed test case for Raindrops challenge

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# exercism-challenges
+# EXERCISM Challenges
+
+<p>This is a source code repository for working through various algorithmic exercises on <a src="exercism.io/">Exercism</a>.<br>Complete with my commented solutions.</p>
+
+## Problems (Golang)
+
+<strong><h3>GOLANG</h3></strong>
+
+- :white_check_mark: \[[PROBLEM]()\] \[[SOLUTION]()\] P1: Counting DNA Nucleotides (DNA)
--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,11 +1,11 @@
-// raindrops.go (STATUS: Pending)
+// raindrops.go (STATUS: Passed.)
 
 package raindrops
 
 import "strconv"
 
 // Convert function returns a select string response based on numerical patterns of its input param.
-// NOTE: Simple solution -- consider using Map object with helper function for optimal quality?
+// NOTE: Uses Map object with helper function for optimal code quality.
 func Convert(number int) string {
 	var Raindrop string
 	var Plelements = map[int]string{
@@ -14,12 +14,14 @@ func Convert(number int) string {
 		7: "Plong",
 	}
 
+	// Iterates through key-value indices in Pl-Elements table
 	for key, value := range Plelements {
 		if number%key == 0 {
 			Raindrop += value
 		}
 	}
 
+	// Final check to convert blank string object appropriately
 	if Raindrop == "" {
 		return strconv.Itoa(number)
 	}
