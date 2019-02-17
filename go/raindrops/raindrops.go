855e87683fc472210c42fdeccfe611a9fde03287
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-02-17 00:48:14 -0800

    Refactored Raindrops code to utilize Map object (DRY solution)

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -7,24 +7,21 @@ import "strconv"
 // Convert function returns a select string response based on numerical patterns of its input param.
 // NOTE: Simple solution -- consider using Map object with helper function for optimal quality?
 func Convert(number int) string {
-	var PlingFactor, PlangFactor, PlongFactor int = 3, 5, 7
-	var PlingEl, PlangEl, PlongEl string = "Pling", "Plang", "Plong"
 	var Raindrop string
+	var Plelements = map[int]string{
+		3: "Pling",
+		5: "Plang",
+		7: "Plong",
+	}
 
-	if number%PlingFactor == 0 {
-		// Pling
-		Raindrop += PlingEl
+	for key, value := range Plelements {
+		if number%key == 0 {
+			Raindrop += value
+		}
 	}
-	if number%PlangFactor == 0 {
-		// Plang
-		Raindrop += PlangEl
-	}
-	if number%PlongFactor == 0 {
-		// Plong
-		Raindrop += PlongEl
-	}
+
 	if Raindrop == "" {
-		Raindrop = strconv.Itoa(number)
+		return strconv.Itoa(number)
 	}
 	return Raindrop
 }
