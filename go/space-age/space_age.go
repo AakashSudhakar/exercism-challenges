a9c676ed4ae1c22793855d514db98cb1dc499cd2
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-02-26 15:05:11 -0800

    Committing successful implementation of Space Age problem

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -2,5 +2,19 @@
 
 package space
 
-// Age function returns
-func Age()
+// Age function returns converted age from seconds to planetary age.
+func Age(seconds float64, planet string) float64 {
+	var Sec2Hr, Hr2Day, Day2Year = 3600.00, 24.00, 365.25
+	var PlanetaryMap = map[string]float64{
+		"Earth":   1.0,
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
+	}
+	var PlanetaryAge = float64(PlanetaryMap[planet])
+	return float64(PlanetaryAge / (Sec2Hr * Hr2Day * Day2Year))
+}
