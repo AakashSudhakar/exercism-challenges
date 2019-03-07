be47faf9301cd6ece517162c03ace53dbdff74f5
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-03-07 03:38:19 -0800

    Implemented Scrabble problem

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,9 @@
 // STATUS: Pending.
+package scrabble
 
+import "strings"
+
+// Global variable storing key-value pairs of letters and associated integer scores
 var scrabbleMap = map[rune]int{
 	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1,
 	'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
@@ -11,6 +15,14 @@ var scrabbleMap = map[rune]int{
 	'Q': 10, 'Z': 10,
 }
 
+// Global function to calculate total score of word
 func Score(word string) int {
+	// Initializing score integer
+	score := 0
 
+	// Maps over letters in word and accumulates score per letter
+	for _, letter := range strings.ToUpper(word) {
+		score += scrabbleMap[letter]
+	}
+	return score
 }
